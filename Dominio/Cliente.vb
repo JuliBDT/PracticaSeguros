da38ce4e32ef39bcc9c6fdc71600111a3a45e9dc
JuliBDT <jcamarero@bdtglobal.com>
2024-10-18 11:42:06 -0300

    calses modelos

---
+++ Dominio/Cliente.vb
@@ -0,0 +1,62 @@
+﻿Public Class Cliente
+    Private _cliente As String
+    Private _nombre As String
+    Private _fechaNacimiento As Date
+    Private _nulldate As Date
+    Private _fechaModificacion As Date
+    Private _estadoCivil As Integer
+
+    Public Property Cliente As String
+        Get
+            Return _cliente
+        End Get
+        Set(value As String)
+            _cliente = value
+        End Set
+    End Property
+
+    Public Property Nombre As String
+        Get
+            Return _nombre
+        End Get
+        Set(value As String)
+            _nombre = value
+        End Set
+    End Property
+
+    Public Property FechaNacimiento As Date
+        Get
+            Return _fechaNacimiento
+        End Get
+        Set(value As Date)
+            _fechaNacimiento = value
+        End Set
+    End Property
+
+    Public Property Nulldate As Date
+        Get
+            Return _nulldate
+        End Get
+        Set(value As Date)
+            _nulldate = value
+        End Set
+    End Property
+
+    Public Property FechaModificacion As Date
+        Get
+            Return _fechaModificacion
+        End Get
+        Set(value As Date)
+            _fechaModificacion = value
+        End Set
+    End Property
+
+    Public Property EstadoCivil As Integer
+        Get
+            Return _estadoCivil
+        End Get
+        Set(value As Integer)
+            _estadoCivil = value
+        End Set
+    End Property
+End Class
---
+++ Dominio/Documento.vb
@@ -0,0 +1,43 @@
+﻿Public Class Documento
+    Private _doctype As Integer
+    Private _documento As String
+    Private _cliente As String
+    Private _fehcaExpiracion As Date
+
+
+    Public Property Doctype As Integer
+        Get
+            Return _doctype
+        End Get
+        Set(value As Integer)
+            _doctype = value
+        End Set
+    End Property
+
+    Public Property Documento As String
+        Get
+            Return _documento
+        End Get
+        Set(value As String)
+            _documento = value
+        End Set
+    End Property
+
+    Public Property Cliente As String
+        Get
+            Return _cliente
+        End Get
+        Set(value As String)
+            _cliente = value
+        End Set
+    End Property
+
+    Public Property FehcaExpiracion As Date
+        Get
+            Return _fehcaExpiracion
+        End Get
+        Set(value As Date)
+            _fehcaExpiracion = value
+        End Set
+    End Property
+End Class
